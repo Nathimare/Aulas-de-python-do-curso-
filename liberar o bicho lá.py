151d35fda8a8e6cb23ab9a2f527558df3dd3ee01
Gabriel Souza <terezamarciaano@gmail.com>
2025-10-07 12:18:11 -0300

    Novas aulas adicionadas

---
+++ liberar o bicho lá.py
@@ -0,0 +1,12 @@
+#Prints
+print("liberar o capeta")
+print("você realmente quer isso?")
+print("Digite 'S' se quer e 'N' se não quiser")
+#Código
+R = input()
+if R == "S":
+    print("O capeta foi liberado👹👹👹👹👹👹👹")
+elif R == "N":
+    print("Não liberou o capeta!😢😢😢😢😢😢")
+else:
+    print("É 'N' ou 'S', macaco!🐵🐵🐵")
